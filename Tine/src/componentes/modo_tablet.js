2299d3b54699d98fbf2e16832f721ff9fc9e9a62
isma4102 <santoscastelvi2014@gmail.com>
2019-09-04 08:54:17 -0300

    modo tablet

--- Tine/src/componentes/modo_tablet.js
+++ Tine/src/componentes/modo_tablet.js
@@ -6,7 +6,7 @@ import PinView from 'react-native-pin-view'
 import { Icon } from 'react-native-elements';
 import moment from "moment";
 import { openDatabase } from 'react-native-sqlite-storage';
-var db = openDatabase({ name: 'sqlliteTesis.db', createFromLocation : 1});
+var db = openDatabase({ name: 'sqlliteTesis.db', createFromLocation: 1 });
 const PendingView = () => (
     <View
         style={{
@@ -53,7 +53,6 @@ export default class modoTablet extends Component {
             mirrorMode: false
         }
         this.perfil();
-
     }
 
 
@@ -61,28 +60,27 @@ export default class modoTablet extends Component {
         Keyboard.dismiss();
         const { codigo } = this.state;
         console.log("prueba");
-        db.transaction(function(tx) {
-            tx.executeSql('SELECT * FROM usuario',[], (tx, results) => {
+        db.transaction(function (tx) {
+            tx.executeSql('SELECT * FROM usuario', [], (tx, results) => {
                 console.log(results.rows.length);
-                for(var i = 0; i < results.rows.length; i++){
+                for (var i = 0; i < results.rows.length; i++) {
                     console.log(results.rows.item(i).pin);
                     console.log(codigo);
-                    if(results.rows.item(i).pin == codigo){
-                        tx.executeSql('SELECT * FROM asistencia WHERE empleado_id = ? AND fin IS NULL',[results.rows.item(i).id], (tx, results) => {
-                            if(results.rowsAffected > 0){
+                    if (results.rows.item(i).pin == codigo) {
+                        tx.executeSql('SELECT * FROM asistencia WHERE empleado_id = ? AND fin IS NULL', [results.rows.item(i).id], (tx, results) => {
+                            if (results.rowsAffected > 0) {
                                 tx.executeSql(
                                     'INSERT INTO asistencia (fin) VALUES (?)',
                                     [],
                                     (tx, results) => {
-                                      console.log('Results', results.rowsAffected);
-                                      if (results.rowsAffected > 0) {
-                                         
-                                        console.log("insertó");
-                                      } else {
-                                          console.log("error");
-                                      }
+                                        console.log('Results', results.rowsAffected);
+                                        if (results.rowsAffected > 0) {
+                                            console.log("insertó");
+                                        } else {
+                                            console.log("error");
+                                        }
                                     }
-                                  );
+                                );
                             }
                         });
                     }
@@ -90,40 +88,40 @@ export default class modoTablet extends Component {
             });
         });
     }
-        /*
-        fetch(server.api + 'login_tablet', {
-            method: 'POST',
-            headers: {
-                'Aceptar': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(loginDetails)
+    /*
+    fetch(server.api + 'login_tablet', {
+        method: 'POST',
+        headers: {
+            'Aceptar': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginDetails)
+    })
+        .then(res => {
+            return res.json()
         })
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                const retorno = data;
-                if (retorno.retorno == true) {
-                    this.state.empleado_id = retorno.id;
-                    if (retorno.estado_ree == 1) {
-                        ToastAndroid.show('Bienvenido, seleccione aceptar', ToastAndroid.LONG);
-                        this.setState({ fin: null });
-                    }
-                    else {
-                        ToastAndroid.show('Buen viaje, seleccione aceptar', ToastAndroid.LONG);
-                        var fin = moment(new Date()).format();
-                        this.setState({ fin: fin });
-                    }
-                } else {
-                    ToastAndroid.show(retorno.mensaje, ToastAndroid.LONG);
+        .then(data => {
+            const retorno = data;
+            if (retorno.retorno == true) {
+                this.state.empleado_id = retorno.id;
+                if (retorno.estado_ree == 1) {
+                    ToastAndroid.show('Bienvenido, seleccione aceptar', ToastAndroid.LONG);
+                    this.setState({ fin: null });
                 }
-            })
-            .catch(function (err) {
-                console.log('error', err);
-            })
+                else {
+                    ToastAndroid.show('Buen viaje, seleccione aceptar', ToastAndroid.LONG);
+                    var fin = moment(new Date()).format();
+                    this.setState({ fin: fin });
+                }
+            } else {
+                ToastAndroid.show(retorno.mensaje, ToastAndroid.LONG);
+            }
+        })
+        .catch(function (err) {
+            console.log('error', err);
+        })
 */
-    
+
 
 
 
@@ -140,22 +138,22 @@ export default class modoTablet extends Component {
 
 
 
-        
-            /*
-            tx.executeSql(
-              'INSERT INTO usuario (documento) VALUES (?)',
-              [1234],
-              (tx, results) => {
-                console.log('Results', results.rowsAffected);
-                if (results.rowsAffected > 0) {
-                   
-                  console.log("insertó");
-                } else {
-                    console.log("error");
-                }
-              }
-            );
-            */
+
+        /*
+        tx.executeSql(
+          'INSERT INTO usuario (documento) VALUES (?)',
+          [1234],
+          (tx, results) => {
+            console.log('Results', results.rowsAffected);
+            if (results.rowsAffected > 0) {
+               
+              console.log("insertó");
+            } else {
+                console.log("error");
+            }
+          }
+        );
+        */
 
 
         let asistencia_send = {
@@ -239,7 +237,7 @@ const styles = StyleSheet.create({
     preview: {
         width: 400,
         height: 400,
-     
+
     },
     capture: {
         flex: 0,
