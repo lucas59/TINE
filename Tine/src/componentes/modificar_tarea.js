4aeffcefb858d8812530c6ab702775ac9b295608
isma4102 <santoscastelvi2014@gmail.com>
2019-10-14 14:37:27 -0300

    modificar tarea

--- Tine/src/componentes/modificar_tarea.js
+++ Tine/src/componentes/modificar_tarea.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ToastAndroid, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, ToastAndroid, ImageBackground } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 const { server } = require('../config/keys');
-import { Button, Icon, Divider, Input } from 'react-native-elements';
+import { TextInput, Button } from 'react-native-paper';
+import {  Icon, Divider } from 'react-native-elements';
 import DateTimePicker from "react-native-modal-datetime-picker";
 import moment from "moment";
 import { openDatabase } from 'react-native-sqlite-storage';
@@ -14,7 +15,7 @@ import BackgroundTimer from 'react-native-background-timer';
 export default class Alta_tarea extends Component {
     static navigationOptions = ({ navigation }) => {
         return {
-            title: 'TINE',
+            title: 'Modificar tarea',
             headerStyle: {
                 backgroundColor: '#00748D',
             },
@@ -63,7 +64,8 @@ export default class Alta_tarea extends Component {
             tarea_id: '',
             tarea_titulo: '',
             tarea_inicio: '',
-            tarea_fin: ''
+            tarea_fin: '',
+            cargando: false
         }
         this.obtener_tarea();
     }
@@ -80,7 +82,7 @@ export default class Alta_tarea extends Component {
     }
 
     saveData = async () => {
-
+        this.setState({ cargando: true });
         const { tarea_titulo, tarea_id, tarea_inicio, tarea_fin } = this.state;
         let modificar_tarea = {
             titulo: tarea_titulo,
@@ -132,7 +134,7 @@ export default class Alta_tarea extends Component {
                 })
 
         }
-
+        this.setState({ cargando: false });
     }
 
     showDateTimePicker_inicio = () => {
@@ -190,43 +192,68 @@ export default class Alta_tarea extends Component {
 
     render() {
         return (
+            <ImageBackground
+            resizeMode='cover'
+            source={require('../imagenes/main.png')}
+            style={{
+              width: '100%',
+              height: '100%',
+              flex: 1
+            }}>
             <View style={styles.container}>
-                <Input
+                <TextInput
+                    label="Titulo de la tarea"
+                    style={{ width: 300, fontSize: 20, marginTop: 30 }}
                     onChangeText={(tarea_titulo) => this.setState({ tarea_titulo })}
-                    value={this.state.tarea_titulo}
+                    placeholder="¿En qué estás trabajando?"
+                    selectionColor="#00748D"
+                    underlineColor="#00748D"
+                    autoFocus={true}
+                    theme={{
+                        colors: {
+                            primary: '#00748D',
+                            underlineColor: 'transparent',
+                        }
 
+                    }}
+                    value={this.state.tarea_titulo}
                 />
                 <Divider style={{ backgroundColor: '#00748D' }} />
                 <Button
-                    title={"Fecha de inicio: " + moment(this.state.tarea_inicio).format('MMMM Do YYYY, HH:mm').toString()}
                     onPress={this.showDateTimePicker_inicio}
-                    type="clear"
-                />
+                    style={{  height: 50, marginTop: 10 }} color="#00748D" mode="outlined"
+                >
                 <DateTimePicker
                     isVisible={this.state.isDateTimePickerVisible_inicio}
                     onConfirm={(date) => this.handleDatePicked_inicio(date)}
                     onCancel={this.hideDateTimePicker_inicio}
                     mode={'datetime'}
                     date={moment(this.state.tarea_inicio).toDate()}
-                />
+                    />
+                    <Text>{"Fecha de inicio: " + moment(this.state.tarea_inicio).format('MMMM Do YYYY, HH:mm:ss').toString()}</Text>
+                </Button>
                 <Divider style={{ backgroundColor: '#00748D' }} />
                 <Button
-                    title={"Fecha de fin: " + moment(this.state.tarea_fin).format('MMMM Do YYYY, HH:mm').toString()}
                     onPress={this.showDateTimePicker_fin}
-                    type="clear"
-                />
+                    style={{  height: 50 , marginTop: 10, marginBottom: 10}} color="#00748D" mode="outlined"
+                >
                 <DateTimePicker
                     isVisible={this.state.isDateTimePickerVisible_fin}
                     onConfirm={(date) => this.handleDatePicked_fin(date)}
                     onCancel={this.hideDateTimePicker_fin}
                     mode={'datetime'}
                     date={moment(this.state.tarea_fin).toDate()}
-                />
+                    />
+                    <Text>{"Fecha de fin: " + moment(this.state.tarea_fin).format('MMMM Do YYYY, HH:mm:ss').toString()}</Text>
+                 </Button>
                 <Divider style={{ backgroundColor: '#00748D' }} />
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText} onPress={this.saveData}>Aceptar</Text>
-                </TouchableOpacity>
-            </View>
+                {this.state.cargando ? <Button style={{ width: 160, height: 50 }} color="#00748D" loading={true} mode="contained" onPress={this.saveData} disabled={true} ></Button> :
+                    <Button style={{ width: 160, height: 50 }} color="#00748D" mode="contained" onPress={this.saveData}>
+                        <Text style={{ fontSize: 23 }}>Aceptar</Text>
+                    </Button>
+                }
+                </View>
+                </ImageBackground>
         )
     }
 }
