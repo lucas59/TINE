1b7996755d3b38a0d4679377a8009a18d3ff5d3d
isma4102 <santoscastelvi2014@gmail.com>
2019-10-16 17:05:51 -0300

    keys

--- Tine/src/config/keys.js
+++ Tine/src/config/keys.js
@@ -1,10 +1,7 @@
 module.exports = {
-  /*  server: {
+   server: {
         api: 'https://servidortesis2019.herokuapp.com/api/',
         img: 'https://servidortesis2019.herokuapp.com/img/perfiles/'
-    }*/
-    server: {
-        api: 'http://192.168.1.45:4005/api/',
-        img: 'https://servidortesis2019.herokuapp.com/img/perfiles/'
     }
+ 
 }
