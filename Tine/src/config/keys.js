3614ba0977103be94e2df815a68a35959b478cec
Lucas Ciceri <lucasciceri59@gmail.com>
2019-10-31 22:17:54 -0300

    Update keys.js

--- Tine/src/config/keys.js
+++ Tine/src/config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-   server: {
-        api: 'https://192.168.1.42:4005/api/',
-        img: 'https://192.168.1.42:4005/img/perfiles/'
-    }
-}
+  server: {
+    api: 'https://servidortesis2019.herokuapp.com/api/',
+    img: 'https://servidortesis2019.herokuapp.com/img/perfiles/',
+  }
+};
